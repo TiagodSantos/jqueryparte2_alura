508c9236f10317e1d0b80f20367be170e24902e1
Tiago dos Santos <suabandeira@gmail.com>
2015-06-08 23:34:22 -0300

    Finalizando implementacao carrinhos (Projeto jQuery)

--- jquery/carrinho.js
+++ jquery/carrinho.js
@@ -1,4 +1,16 @@
-
+var umaPropaganda = function(){
+	var propagandas = ["O que acha de comprar uma motocicleta?",
+	                   "O que acha de comprar uma lancha?",
+	                   "O que acha de comprar uma bicicleta?",
+	                   "O que acha de comprar um carro?"
+	                   ];
+	
+	var posicao = Math.floor(propagandas.length * Math.random());
+	var texto = propagandas[posicao];
+	var tr = $("<tr>").addClass("propaganda").append($("<td>"));
+	tr.find("td").attr("colspan", 6).text(texto);
+	return tr;
+}
 
 //Funcao para atualizar dadoas
 var atualizaDados = function() {
@@ -66,6 +78,22 @@ var undo = function(){
 	
 };
 
+var daDestaque = function(){
+	$(this).find(".remove-item").fadeIn();
+	$(this).addClass("hovering");
+}
+
+var tiraDestaque = function(){
+	$(this).find(".remove-item").fadeOut();
+	$(this).removeClass("hovering");
+}
+
+var alternaPropagandas = function(event){
+	event.preventDefault();
+	$(".propaganda").fadeToggle();
+	$(".alterna-propaganda").toggle();
+}
+
 // Funcao aposInicializado
 var aposInicializado = function() {
 
@@ -74,7 +102,16 @@ var aposInicializado = function() {
 	$(".remove-item").click(removeItem);
 	//Define comportamento para qdo houver o clique no botao undo
 	$(".undo").click(undo);
-
+	
+	$(".carrinho").each(function(){
+		$(this).find("tr:nth-child(3n)").each(function(){
+			umaPropaganda().insertAfter($(this));
+		});
+	});
+	$(".carrinho tbody tr").hover(daDestaque, tiraDestaque);
+	$(".alterna-propaganda").click(alternaPropagandas);
+	
+	
 };
 
 /*
